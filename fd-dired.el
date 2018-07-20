fba87703dd9825e6967f3b2e8dff0e29c9a28406
yqrashawn <namy.19@gmail.com>
2018-07-20 20:14:21 +0800

    hash for compiler

--- fd-dired.el
+++ fd-dired.el
@@ -100,7 +100,7 @@ use in place of \"-ls\" as the final argument."
     (dired-mode dir (cdr fd-dired-ls-option))
     (let ((map (make-sparse-keymap)))
       (set-keymap-parent map (current-local-map))
-      (define-key map "\C-c\C-k" 'kill-find)
+      (define-key map "\C-c\C-k" #'kill-find)
       (use-local-map map))
     (make-local-variable 'dired-sort-inhibit)
     (setq dired-sort-inhibit t)
